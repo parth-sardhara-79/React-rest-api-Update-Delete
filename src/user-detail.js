389b49bc1fce9fa9574c89ab9c483e1d2e2f12b2
parth-sardhara-79 <parthsardhara026@gmail.com>
2020-02-06 17:45:58 +0530

    Final Commit

--- src/user-detail.js
+++ src/user-detail.js
@@ -5,7 +5,7 @@ import Modal from 'react-bootstrap/Modal';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
 
 class UserDetail extends React.Component {
 
